e78f99320807e59c0eb6bd00d2a5578e09f0d302
apple <nikitosiksymonenko@gmail.com>
2025-04-24 00:29:14 +0200

    app final

---
+++ app/src/main/java/com/example/weatherapp/HourlyModel.kt
@@ -0,0 +1,7 @@
+package com.example.weatherapp
+
+data class HourlyModel(
+    val hour: String,
+    val temp: Int,
+    val picPath: String
+)
--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -4,40 +4,34 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.annotation.ContentView
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.material3.Text
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-
+            WeatherScreen()
         }
     }
 }
@@ -55,116 +49,205 @@ fun WeatherScreen() {
                         Color(android.graphics.Color.parseColor("#643d67")),
                     )
                 )
-            ))
-            {
-                Column(modifier = Modifier.fillMaxSize()) {
-                    LazyColumn(modifier = Modifier.fillMaxSize(),
-                        horizontalAlignment = Alignment.CenterHorizontally) {
+            )
+    ) {
+        Column(modifier = Modifier.fillMaxSize()) {
+            LazyColumn(
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                item {
+                    Text(
+                        text = "Mostly Cloudy",
+                        fontSize = 20.sp,
+                        color = Color.White,
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(top = 48.dp),
+                        textAlign = TextAlign.Center
+                    )
 
-                        item{
-                            Text(text = "Mostly Cloudy",
-                                fontSize = 20.sp,
-                                color = Color.White,
-                                modifier = Modifier
-                                    .fillMaxSize()
-                                    .padding(top = 48.dp),
-                                textAlign = TextAlign.Center
-                           )
+                    Image(
+                        painter = painterResource(id = R.drawable.cloudy_sunny),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .size(150.dp)
+                            .padding(top = 8.dp)
+                    )
 
-                            Image(painter = painterResource(id = R.drawable.cloudy_sunny)
-                            ,contentDescription = null,
-                                modifier = Modifier
-                                    .size(150.dp)
-                                    .padding(top = 8.dp)
+                    Text(
+                        text = "Today, 12.00 PM",
+                        fontSize = 19.sp,
+                        color = Color.White,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(top = 8.dp),
+                        textAlign = TextAlign.Center
+                    )
+
+                    Text(
+                        text = "25c",
+                        fontSize = 63.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.White,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(top = 8.dp),
+                        textAlign = TextAlign.Center
+                    )
+
+                    Text(
+                        text = "H:27 L:18",
+                        fontSize = 16.sp,
+                        color = Color.White,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(top = 8.dp),
+                        textAlign = TextAlign.Center
+                    )
+
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 24.dp, vertical = 16.dp)
+                            .background(
+                                color = colorResource(id = R.color.purple),
+                                shape = RoundedCornerShape(25.dp)
                             )
-
-                            // Dispay date and time
-                            Text(text = "Today, 12.00 PM",
-                                fontSize = 19.sp,
-                                color = Color.White,
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .padding(top = 8.dp),
-                                textAlign = TextAlign.Center
-                           )
-
-                            // Display temperature
-                            Text(text = "25c"
-                                , fontSize = 63.sp,
-                                fontWeight = FontWeight.Bold,
-                                color = Color.White,
-                                modifier = Modifier.fillMaxWidth()
-                                    .padding(top = 8.dp),
-                                textAlign = TextAlign.Center
-                            )
-
-                            Text(text = "H:27 L:18",
-                                fontSize = 16.sp,
-                                color = Color.White,
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .padding(top = 8.dp),
-                                textAlign = TextAlign.Center
-                           )
-
-                            //Weather Box
-
-                            Box(modifier = Modifier
+                    ) {
+                        Row(
+                            modifier = Modifier
                                 .fillMaxWidth()
-                                .padding(horizontal = 24.dp, vertical = 16.dp)
-                                .background(
-                                    color = colorResource(id = R.color.purple),
-                                    shape = RoundedCornerShape(25.dp)
-                                )
-                            ){
-                                Row(modifier = Modifier
-                                    .fillMaxWidth()
-                                    .height(100.dp)
-                                    .padding(horizontal = 8.dp),
-                                    verticalAlignment =  Alignment.CenterVertically,
-                                    horizontalArrangement = Arrangement.SpaceBetween
-                                ) {
-                                    WeatherDetailItem(
-                                        icon = R.drawable.rain,
-                                        value = "22%",
-                                        label = "Rain"
-                                    )
-                                    WeatherDetailItem(
-                                        icon = R.drawable.wind,
-                                        value = "26%",
-                                        label = "Wind Speed"
-                                    )
-                                    WeatherDetailItem(
-                                        icon = R.drawable.humidity,
-                                        value = "18%",
-                                        label = "Humidity"
-                                    )
-                                }
-                            }
-
-                            // Display weather details
-
+                                .height(100.dp)
+                                .padding(horizontal = 8.dp),
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.SpaceBetween
+                        ) {
+                            WeatherDetailItem(
+                                icon = R.drawable.rain,
+                                value = "22%",
+                                label = "Rain"
+                            )
+                            WeatherDetailItem(
+                                icon = R.drawable.wind,
+                                value = "26%",
+                                label = "Wind Speed"
+                            )
+                            WeatherDetailItem(
+                                icon = R.drawable.humidity,
+                                value = "18%",
+                                label = "Humidity"
+                            )
                         }
                     }
-       }
+
+                    Text(
+                        text = "Today's Weather",
+                        fontSize = 20.sp,
+                        color = Color.White,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 24.dp, vertical = 8.dp),
+                    )
+                }
+
+                item {
+                    LazyRow(
+                        modifier = Modifier.fillMaxWidth(),
+                        contentPadding = PaddingValues(horizontal = 20.dp),
+                        horizontalArrangement = Arrangement.spacedBy(4.dp)
+                    ) {
+                        items(items) { item ->
+                            FutureModelViewHolder(model = item)
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
 @Composable
-fun WeatherDetailItem(icon:Int, value:String, label:String) {
-    Column(modifier = Modifier.padding(16.dp)
-        ,horizontalAlignment = Alignment.CenterHorizontally,
+fun FutureModelViewHolder(model: HourlyModel) {
+    Column(
+        modifier = Modifier
+            .width(90.dp)
+            .wrapContentHeight()
+            .padding(4.dp)
+            .background(
+                color = colorResource(id = R.color.purple),
+                shape = RoundedCornerShape(8.dp)
+            )
+            .padding(8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        Image(painter = painterResource(id = icon)
-            , contentDescription = null,
+        Text(
+            text = model.hour,
+            color = Color.White,
+            fontSize = 16.sp,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 8.dp),
+            textAlign = TextAlign.Center
+        )
+
+        Image(
+            painter = painterResource(
+                id = when (model.picPath) {
+                    "cloudy" -> R.drawable.cloudy
+                    "sunny" -> R.drawable.sunny
+                    "wind" -> R.drawable.wind
+                    "rainy" -> R.drawable.rain
+                    "cloudy_sunny" -> R.drawable.cloudy
+                    else -> R.drawable.sunny
+                }
+            ),
+            contentDescription = null,
+            modifier = Modifier
+                .size(50.dp)
+                .padding(bottom = 8.dp),
+            contentScale = ContentScale.Crop
+        )
+
+        Text(
+            text = "${model.temp}°",
+            color = Color.White,
+            fontSize = 16.sp,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 8.dp),
+            textAlign = TextAlign.Center
+        )
+    }
+}
+
+val items = listOf(
+    HourlyModel(hour = "9 PM", temp = 25, picPath = "cloudy"),
+    HourlyModel(hour = "10 PM", temp = 29, picPath = "sunny"),
+    HourlyModel(hour = "11 PM", temp = 24, picPath = "wind"),
+    HourlyModel(hour = "12 PM", temp = 21, picPath = "rainy"),
+    HourlyModel(hour = "1 AM", temp = 27, picPath = "cloudy")
+)
+
+@Composable
+fun WeatherDetailItem(icon: Int, value: String, label: String) {
+    Column(
+        modifier = Modifier.padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Image(
+            painter = painterResource(id = icon),
+            contentDescription = null,
             modifier = Modifier.size(34.dp)
         )
-        Text(text = value,
+        Text(
+            text = value,
             fontWeight = FontWeight.Bold,
             color = colorResource(id = R.color.white),
             textAlign = TextAlign.Center
-       )
-        Text(text = label,
+        )
+        Text(
+            text = label,
             color = colorResource(id = R.color.white),
             textAlign = TextAlign.Center
         )
