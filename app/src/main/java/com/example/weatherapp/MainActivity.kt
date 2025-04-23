972dc4970b87a736391551bf81a5c256f446bd06
apple <nikitosiksymonenko@gmail.com>
2025-04-23 18:18:04 +0200

    Commit

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -4,14 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.weatherapp.ui.theme.WeatherAppTheme
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.material3.Text
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,6 +39,60 @@ class MainActivity : ComponentActivity() {
 @Preview
 @Composable
 fun WeatherScreen() {
-    Box(modifier = Modifier
-        .fillMaxSize())
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                brush = Brush.horizontalGradient(
+                    colors = listOf(
+                        Color(android.graphics.Color.parseColor("#59469d")),
+                        Color(android.graphics.Color.parseColor("#643d67")),
+                    )
+                )
+            ))
+            {
+                Column(modifier = Modifier.fillMaxSize()) {
+                    LazyColumn(modifier = Modifier.fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally) {
+
+                        item{
+                            Text(text = "Mostly Cloudy",
+                                fontSize = 20.sp,
+                                color = Color.White,
+                                modifier = Modifier
+                                    .fillMaxSize()
+                                    .padding(top = 48.dp),
+                                textAlign = TextAlign.Center
+                           )
+
+                            Image(painter = painterResource(id = R.drawable.cloudy_sunny)
+                            ,contentDescription = null,
+                                modifier = Modifier
+                                    .size(150.dp)
+                                    .padding(top = 8.dp)
+                            )
+
+                            // Dispay date and time
+                            Text(text = "Today, 12.00 PM",
+                                fontSize = 19.sp,
+                                color = Color.White,
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(top = 8.dp),
+                                textAlign = TextAlign.Center
+                           )
+
+                            // Display temperature
+                            Text(text = "25c"
+                                , fontSize = 63.sp,
+                                fontWeight = FontWeight.Bold,
+                                color = Color.White,
+                                modifier = Modifier.fillMaxWidth()
+                                    .padding(top = 8.dp),
+                                textAlign = TextAlign.Center
+                            )
+                        }
+                    }
+       }
+    }
 }
